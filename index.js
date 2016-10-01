2b6fe349713713ccef826a0427700d23a30de83f
zion <guzhaoyuan@126.com>
2016-10-01 13:52:58 +0800

    v1.0 finished,code clean

--- index.js
+++ index.js
@@ -9,35 +9,35 @@ app.use(bodyParser.json());
 
 app.use(express.static('homepage'));
 
-app.post('/homepage',function (req, res) {
+app.post('/',function (req, res) {
     console.log(req.body);
 
-    // create reusable transporter object using the default SMTP transport
-    var transporter = nodemailer.createTransport('smtps://tiankonggongchang%40gmail.com:tiankonggongchang1@smtp.gmail.com');
+//     // create reusable transporter object using the default SMTP transport
+//     var transporter = nodemailer.createTransport('smtps://tiankonggongchang%40gmail.com:tiankonggongchang1@smtp.gmail.com');
+//
+// //setup e-mail data with unicode symbols
+//     var mailOptions = {
+//         from: req.body.name + req.body.email, // sender address
+//         to: 'guzhaoyuan@126.com', // list of receivers
+//         subject: '[Homepage Message]', // Subject line
+//         text: req.body.message, // plaintext body
+//     };
+//
+// // send mail with defined transport object
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log('Message sent: ' + info.response);
+//         }
+//     });
 
-//setup e-mail data with unicode symbols
-    var mailOptions = {
-        from: req.body.name + req.body.email, // sender address
-        to: 'guzhaoyuan@126.com', // list of receivers
-        subject: '[Homepage Message]', // Subject line
-        text: req.body.message, // plaintext body
-    };
-
-// send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Message sent: ' + info.response);
-        }
-    });
-
-    //res.sendStatus(200);
-    res.redirect("/contact.html");
+    res.sendStatus(200);
+    //res.redirect("/contact.html");
     //res.end('{"success" : "Updated Successfully", "status" : 200}');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('homepage listening on port 3000!');
 });
 
